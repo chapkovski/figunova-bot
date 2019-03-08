63d3e34c8292cc9a5bbaa37f8c61501c45fbb87c
chapkovski <chapkovski@gmail.com>
2019-03-08 12:45:56 +0300

    Филька!!! 0.4820787368065198 Не забудь написать коммит!

--- access_gsheet.py
+++ access_gsheet.py
@@ -4,7 +4,7 @@ from gspread.models import CellNotFound
 from datetime import date, datetime
 from utils import cp
 from json import dumps
-from gspread.exceptions import CellNotFound,APIError
+from gspread.exceptions import CellNotFound, APIError
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file",
           "https://www.googleapis.com/auth/drive"]
@@ -12,6 +12,8 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', S
 
 client = gspread.authorize(creds)
 sheet = client.open("budget").sheet1
+
+
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
 
@@ -22,12 +24,16 @@ def json_serial(obj):
 
 def gsheet_register_payment(date, user_id, update_id, user_name, val, rest, ):
     date = dumps(date, default=json_serial)
-
-    #
-    # Extract and print all of the values
-    row = [date, user_id, user_name, val, rest,update_id]
+    #    # Extract and print all of the values
+    row = [date, user_id, user_name, val, rest, update_id]
     index = 2
-    sheet.insert_row(row, index)
+    try:
+        client = gspread.authorize(creds)
+        sheet = client.open("budget").sheet1
+        sheet.insert_row(row, index)
+    except (CellNotFound, APIError):
+        cp('SOMETHING IS WRONG WITH PAYMENT REGISTRATION!!!!')
+        return False
 
 
 def delete_gsheet_record(update_id):
@@ -36,10 +42,9 @@ def delete_gsheet_record(update_id):
         row_number = cell.row
         sheet.delete_row(row_number)
         return True
-    except (CellNotFound,APIError ):
+    except (CellNotFound, APIError):
         cp('SOMETHING IS WRONG!!!!')
         return False
-
 
 
 if __name__ == '__main__':
