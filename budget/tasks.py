c346466906a800e3409ca256a9335db03d395e24
chapkovski <chapkovski@gmail.com>
2019-04-05 20:42:09 +0300

    retry to task

--- budget/tasks.py
+++ budget/tasks.py
@@ -1,14 +1,15 @@
 from transactions.gsheets import update_record
 import logging
+
 logger = logging.getLogger(__name__)
-from huey.contrib.djhuey import  task
+from huey.contrib.djhuey import task
 
 
-@task()
+@task(retries=2, retry_delay=10)
 def huey_update_record(i):
     r = update_record(i)
     if r:
         logger.info('successfully update ghseet record')
     else:
-        logger.warning('failed to update gsheet record')
+        raise Exception('failed to update gsheet record')
     return r
